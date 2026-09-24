e59ae0cc546acd4d7d6456a95bbf1a898d519892
Bannysv26 <bannysriram05@gmail.com>
2026-09-24 08:52:21 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,7 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAwreGA_Ni0om_TykrL8s7YCIV97RxLsrU",
   authDomain: "pharmacyguides26-talent.firebaseapp.com",
@@ -6,3 +10,9 @@ const firebaseConfig = {
   messagingSenderId: "789527078072",
   appId: "1:789527078072:web:8bb2c132a6ae83a7b4404d"
 };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
