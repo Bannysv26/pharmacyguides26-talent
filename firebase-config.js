064fd3f01286805754bb34d049775ed631810bde
Bannysv26 <bannysriram05@gmail.com>
2026-09-24 08:48:20 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,8 @@
-
+const firebaseConfig = {
+  apiKey: "AIzaSyAwreGA_Ni0om_TykrL8s7YCIV97RxLsrU",
+  authDomain: "pharmacyguides26-talent.firebaseapp.com",
+  projectId: "pharmacyguides26-talent",
+  storageBucket: "pharmacyguides26-talent.firebasestorage.app",
+  messagingSenderId: "789527078072",
+  appId: "1:789527078072:web:8bb2c132a6ae83a7b4404d"
+};
